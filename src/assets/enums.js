b8fe8d311269be459de5ca2b431cedad330752e6
Thomas Chia <thomas.chia@amaas.com>
2017-10-27 12:38:36 +0800

    Fix lodash import. AMAAS-1874

--- src/assets/enums.js
+++ src/assets/enums.js
@@ -1,5 +1,3 @@
-import forEach from 'lodash/forEach'
-
 export const OPTION_STYLES = ['American', 'Bermudan', 'European']
 export const OPTION_TYPES = ['Put', 'Call']
 export const FUND_TYPES = ['Open', 'Closed', 'ETF']
--- src/exports/assets.js
+++ src/exports/assets.js
@@ -1,3 +1,5 @@
+import forEach from 'lodash/forEach'
+
 /**
  * Classes for the Assets service
  * @module assets
