aed9c1ec334b9489879805404729c0ace73e3666
Max Goh <max.goh@argomi.com>
2017-10-30 19:03:43 +0800

    Added file cashTransactions.js

---
+++ src/transactions/Transaction/cashTransactions.js
@@ -0,0 +1,57 @@
+import Transaction from './transaction'
+
+class CashTransaction extends Transaction {
+  constructor({
+    assetManagerId,
+    assetBookId,
+    counterpartyBookId,
+    transactionAction,
+    assdetId,
+    quantity,
+    transactionDate,
+    settlementDate,
+    price=1,
+    transactionCurrency,
+    settlementCurrency,
+    assets,
+    executionTime,
+    transactionType,
+    transactionId,
+    transactionStatus,
+    charges,
+    codes,
+    comments,
+    links,
+    parties,
+    rates,
+    references
+  }) {
+    super({
+      assetManagerId,
+      assetBookId,
+      counterpartyBookId,
+      transactionAction,
+      assdetId,
+      quantity,
+      transactionDate,
+      settlementDate,
+      price: 1,
+      transactionCurrency,
+      settlementCurrency,
+      assets,
+      executionTime,
+      transactionType,
+      transactionId,
+      transactionStatus,
+      charges,
+      codes,
+      comments,
+      links,
+      parties,
+      rates,
+      references
+    })
+  }
+}
+
+export default CashTransaction
