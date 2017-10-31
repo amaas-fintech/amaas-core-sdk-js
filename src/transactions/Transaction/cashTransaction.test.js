008d8e8acd91892c6ac45e9369f5802d68cf0dc6
Max Goh <max.goh@argomi.com>
2017-10-31 12:10:10 +0800

    Added Test Case for CashTransaction

---
+++ src/transactions/Transaction/cashTransaction.test.js
@@ -0,0 +1,14 @@
+import CashTransaction from './cashTransaction.js'
+
+const Decimal = require('decimal.js')
+
+describe('CashTransaction class', () => {
+  let testCashTransaction
+  beforeEach(() => {
+    const data = { assetManagerId: 123 }
+    testCashTransaction = new CashTransaction(data)
+  })
+  it('should set Price correctly', () => {
+    expect(testCashTransaction.price).toEqual(1) 
+  })
+})
