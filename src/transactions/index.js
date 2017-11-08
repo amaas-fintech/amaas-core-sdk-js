bbb294c24135ac8a42fdb0ed6124d136b892ca80
Max Goh <max.goh@argomi.com>
2017-11-08 16:46:27 +0800

    Updated transaction/index.js

--- src/transactions/index.js
+++ src/transactions/index.js
@@ -1,3 +1,4 @@
 export { default as Transaction } from './Transaction/transaction.js'
 export { default as Position } from './Positions/position.js'
 export { default as PNLResult } from './PNLResult/PNLResult.js'
+export { default as CashTransaction } from './Transaction/cashTransaction.js'
