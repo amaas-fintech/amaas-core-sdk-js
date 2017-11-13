d8ee49131b0a0384003d141967e6190f9f6bd2b6
Max Goh <max.goh@argomi.com>
2017-11-13 13:12:24 +0800

    Remove duplicate line

--- src/transactions/index.js
+++ src/transactions/index.js
@@ -1,7 +1,6 @@
 export { default as Transaction } from './Transaction/transaction.js'
 export { default as Position } from './Positions/position.js'
 export { default as PNLResult } from './PNLResult/PNLResult.js'
-export { default as PNLResult } from './PNLResult/PNLResult.js'
 export { default as CashTransaction } from './Transaction/cashTransaction.js'
 export { default as PositionPNL } from './PostionPNL/PositionPNL'
 export { default as TransactionPNL } from './TransactionPNL/TransactionPNL'
